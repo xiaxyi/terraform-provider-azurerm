eee0f7650a11ac368ed966d5e04c03da5e4747b0
Anurag Upadhaya <anurag.upadhaya@hashicorp.com>
2026-04-22 11:57:01 +0530

    service/storagemover: add list support for `azurerm_storage_mover_agent` (#32166)

--- internal/services/storagemover/registration.go
+++ internal/services/storagemover/registration.go
@@ -67,6 +67,7 @@ func (r Registration) EphemeralResources() []func() ephemeral.EphemeralResource 
 
 func (r Registration) ListResources() []sdk.FrameworkListWrappedResource {
 	return []sdk.FrameworkListWrappedResource{
+		StorageMoverAgentListResource{},
 		StorageMoverListResource{},
 	}
 }
